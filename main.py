57197b5405c76c5629e6bc88f80af796f4a1d72f
mohammad-ext-nuri <mohammad.rahman.ext@bitwala.com>
2022-06-25 05:33:09 +0200

    stashed changes
    all

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import warnings
 import os
 from argparse import ArgumentParser
+<<<<<<< Updated upstream
 from os import listdir
 from os.path import isfile, join
 from pathlib import Path
@@ -8,6 +9,12 @@ from typing import List, Union
 from utils.config import path_config
 from utils.files import load_sound_file
 from utils.compute import load_audio, compute, compare, score, normalise_score
+=======
+from utils.config import path_config
+from utils.files import load_sound_file
+from utils.compute import score, normalise_score
+from utils.predict import predict_audio_class
+>>>>>>> Stashed changes
 
 
 
@@ -15,7 +22,12 @@ from utils.compute import load_audio, compute, compare, score, normalise_score
 def main()->float:
     input_file_path = load_sound_file('input')
     input_file_path = input_file_path[0]
+<<<<<<< Updated upstream
     raw_score = score(input_file_path, path_config['train'])
+=======
+
+    raw_score = score(input_file_path, path_config['benchmark'])
+>>>>>>> Stashed changes
     final_score = normalise_score(raw_score)
     print(f'Your hamba match is {final_score:.2%}')
     return final_score
